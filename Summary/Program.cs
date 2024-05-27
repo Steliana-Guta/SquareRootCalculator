57652f46e3c9bef5b473ed463407bd04f7ea8e5e
Steliana-Guta <steliana.m.guta@gmail.com>
2024-05-27 19:00:29 +0100

    Add project files.

---
+++ Summary/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Summary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number;
+            double squareroot;
+
+            Console.WriteLine("Hello, please insert a number so i can calculate the square root for you.");
+
+            number = int.Parse(Console.ReadLine());
+            squareroot = Math.Sqrt(number);
+            Console.WriteLine("The square root of the number you inserted is " + squareroot);
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
